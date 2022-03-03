cd1588841858e783fe2818d425d14ca519fae582
atos-hardik-benani <97894344+atos-hardik-benani@users.noreply.github.com>
2022-03-03 22:48:55 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,33 +22,14 @@ variable "volume_type" {
   default     = "gp2"
 }
 
-# Tags for the EC2 instance
-variable "request_it" {
-  description = "Internal request_it number"
-  type        = string
-  default     = ""
-}
-
-variable "cost_center" {
-  description = "Internal cost center id used for billing purposes"
-  type        = string
-  default     = ""
-}
-
 variable "created_by_email" {
   description = "Email address of the resourse owner/creator"
   type        = string
   default     = "test_email@test.com"
 }
 
-
 variable "key_pair_name" {
   description = "Key name of the Key Pair to use for the instance."
-  default     = ""
-}
-
-variable "ssh_public_key" {
-  description = "Key pair's public key will be registered with AWS to allow logging-in to EC2 instances."
   default     = ""
 }
 
