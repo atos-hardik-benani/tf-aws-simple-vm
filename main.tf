1a2f4c1d037ff0790cab9d614b4ba3cb9f42d13e
atos-hardik-benani <97894344+atos-hardik-benani@users.noreply.github.com>
2022-03-03 22:51:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,10 @@ resource "aws_instance" "this_instance" {
   instance_type               = lower(var.instance_type)
   subnet_id                   = data.aws_subnet.this_subnet.id
   associate_public_ip_address = false
-  key_name                    = var.ssh_public_key != "" ? aws_key_pair.pub_key[0].id : var.key_pair_name
+  key_name                    = var.key_pair_name
 
   tags = {
     Name             = "instance_workspace_${substr("${terraform.workspace}", 4, -1)}"
-    RequestIT        = var.request_it,
-    Cost_Center      = var.cost_center,
     Created_By_Email = var.created_by_email
   }
 
@@ -17,12 +15,6 @@ resource "aws_instance" "this_instance" {
     volume_size = var.volume_size
     volume_type = var.volume_type
   }
-}
-
-resource "aws_key_pair" "pub_key" {
-  count      = var.ssh_public_key != "" ? 1 : 0
-  key_name   = "${var.key_pair_name}-${substr("${terraform.workspace}", 4, -1)}"
-  public_key = var.ssh_public_key
 }
 
 
