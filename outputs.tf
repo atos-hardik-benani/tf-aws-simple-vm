b2dda716db701f1cc1375f27b89e92a12fc12c05
atos-hardik-benani <97894344+atos-hardik-benani@users.noreply.github.com>
2022-03-03 22:52:17 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,6 @@ output "private_ip" {
   value = aws_instance.this_instance.private_ip
 }
 
-/*
-output "ssh_key_kame" {
-  value = aws_instance.this_instance.key_name
-}
-*/
-
 output "instance_type" {
   value = aws_instance.this_instance.instance_type
 }
@@ -29,16 +23,6 @@ output "volume_size" {
 
 output "volume_type" {
   value = aws_instance.this_instance.root_block_device.0.volume_type
-}
-
-
-
-output "request_it" {
-  value = aws_instance.this_instance.tags.RequestIT
-}
-
-output "cost_center" {
-  value = aws_instance.this_instance.tags.Cost_Center
 }
 
 output "created_by_email" {
