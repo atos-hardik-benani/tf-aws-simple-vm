41096a34701a60145ac33f3b1d786b27df19b7bc
atos-hardik-benani <97894344+atos-hardik-benani@users.noreply.github.com>
2022-03-06 00:49:34 +0000

    Update data-dependency.tf

--- data-dependency.tf
+++ data-dependency.tf
@@ -1,11 +1,3 @@
-# Search by subnet Name
-data "aws_subnet" "this_subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet_name]
-  }
-}
-
 # Data source to obtain dynamically the ami id
 # If the user wants to use another OS selecting a different one from servicenow...
 # ...we should add more variables for that. But it's going to be a challenge to filter the ami using only the name of the OS
